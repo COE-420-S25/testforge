873cd35ff18fa54a0c06ed4c5a3b70cd545c0a82
unknown <omarelshall2005@gmail.com>
2025-03-24 04:43:14 +0400

    Added package

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve all static files from public/
+// Serve static files from /public
 app.use(express.static(path.join(__dirname, "public")));
+
+// Serve proposal folder statically at /proposal
+app.use("/proposal", express.static(path.join(__dirname, "proposal")));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
