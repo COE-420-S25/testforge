72132b4462bd3aa4d399cd82e1eac3ecf2657ab5
unknown <omarelshall2005@gmail.com>
2025-03-24 04:38:06 +0400

    Added package

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-// server.js
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (CSS, JS, images) from public folder
+// Serve all static files from public/
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve the proposal page
-app.use("/proposal", express.static(path.join(__dirname, "proposal")));
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("Welcome to TestForge!");
-});
-
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
